d09fb860868ad34224ae9569ef360c7588cb4440
Thomas Karpati <tkarpati@foodclaw.net>
2019-06-03 14:52:46 -0700

    Adding conv1d to lstm model

---
+++ keras_models/lstm_model_1d.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+
+from keras_models import keras_model
+
+# Implement baseline in Keras
+class LstmModel1D(keras_model.KerasModel):
+    
+  def __init__(self,
+               hparams,
+               model_dir,
+               verbose=False):
+
+    super().__init__(hparams, model_dir, verbose)
+
+    # reshape and squeeze this to be [num_samples, sequence, features]
+    def lstm_reshape_layer(x):
+      return tf.transpose(x, perm=[0, 2, 1])
+    
+    X_in = tf.keras.layers.Input(shape=self.hparams['input_shape'], name='input')
+    x = tf.keras.layers.Lambda(lstm_reshape_layer)(X_in)
+    if self.hparams['batch_norm']: x = tf.keras.layers.BatchNormalization()(x)
+    
+    # Repeat 4 times
+    for i in range(self.hparams['num_conv_layers']):
+      # Call conv1d (we have powers of 2 increase each iteration)
+      # This trains single kernel over all frequencies
+      nfilters = 16 * (2 ** i)
+      x = tf.keras.layers.Conv1D(filters=nfilters,
+                                 kernel_size=3,
+                                 strides=1,
+                                 use_bias=False,
+                                 activation=None,
+                                 padding='same',
+                                 name='conv1d_h{}'.format(i))(x)
+      # Run batch normalization per conv layer before activation
+      if self.hparams['batch_norm']: x = tf.keras.layers.BatchNormalization()(x)
+      x = tf.keras.layers.Activation('relu')(x)
+
+    # LSTM layer
+    for i in range(self.hparams['num_lstm_hidden_layers']):
+      # Call lstm layer multiple times as needed
+      x = tf.keras.layers.LSTM(128,
+                               name='lstm_h{}'.format(i))(x)
+      
+    x = tf.keras.layers.LSTM(128,
+                             name='lstm')(x)
+    
+    # Get the logits for softmax output
+    logits = tf.keras.layers.Dense(self.hparams['num_classes'],
+                                   activation='softmax',
+                                   use_bias=True,
+                                   name='dense_softmax')(x)
+
+    self.model = tf.keras.models.Model(inputs=X_in, outputs=logits)
+
+
+    self.compile()
+
---
+++ run_keras_lstm_model_1d.py
@@ -0,0 +1,101 @@
+import sys
+import numpy as np
+import tensorflow as tf
+
+import argparse
+import datetime
+
+from data_processing import generate_data_sets
+from data_processing import keras_data_generator
+from data_processing import utils
+
+from keras_models import lstm_model_1d
+
+print("Using python version:", sys.version)
+print("Using numpy version:", np.__version__)
+print("Using TensorFlow version:", tf.__version__)
+
+
+utils.log_timestamp()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="Verbose output", action="store_true")
+parser.add_argument("-i", "--interactive", help="Load data and models and exit", action="store_true")
+parser.add_argument("-f", "--fast", help="Run validation vs test (no training data processed)", action="store_true")
+parser.add_argument("-data_dir", help="Input data location", default="./data")
+parser.add_argument("-model_dir", help="Output data file location", default="./model")
+args = parser.parse_args()
+
+hparams = {
+    'batch_size': 64,
+    #'keep_prob': 0.5,
+    #'batch_norm': True,
+    'keep_prob': 1.0,
+    'batch_norm': False,
+    'num_conv_layers': 1,
+    'num_lstm_hidden_layers': 0,
+    'epochs': 10,
+    'gen_spectrogram': True,
+    'spectrogram_params': None,
+    'loss': 'categorical_crossentropy',
+    'optimizer': 'adam',
+    'multiprocess': False,
+    'threads': 1,
+}
+
+
+# Get the information about the input data shape and the number of classes from
+# the data. This will get passed to the model when we generate it.
+hparams['input_shape'] = keras_data_generator.get_input_shape(hparams)
+hparams['num_classes'] = keras_data_generator.get_num_classes()
+
+#data_generator_verbose = args.verbose
+data_generator_verbose = False
+
+# Use the project data processing directives
+utils.log_timestamp()
+if not args.fast:
+  training_data = generate_data_sets.read_dataset(name="training",
+                                                  in_dir=args.data_dir)
+  training_sequence = keras_data_generator.DataSequence(training_data,
+                                                        hparams=hparams,
+                                                        shuffle=True,
+                                                        verbose=data_generator_verbose)
+
+validation_data = generate_data_sets.read_dataset(name="validation",
+                                                  in_dir=args.data_dir)
+validation_sequence = keras_data_generator.DataSequence(validation_data,
+                                                        hparams=hparams,
+                                                        shuffle=False,
+                                                        verbose=data_generator_verbose)
+
+test_data = generate_data_sets.read_dataset(name="test",
+                                            in_dir=args.data_dir)
+test_sequence = keras_data_generator.DataSequence(test_data,
+                                                  hparams=hparams,
+                                                  shuffle=False,
+                                                  verbose=data_generator_verbose)
+
+if args.fast:
+  training_sequence = test_sequence
+
+# Create the model and compile
+utils.log_timestamp()
+model = lstm_model_1d.LstmModel1D(hparams,
+                                  model_dir=args.model_dir,
+                                  verbose=args.verbose)
+
+if args.interactive: exit()
+
+utils.log_timestamp()
+print("Starting training....")
+history = model.train(training_sequence,
+                      validation_sequence,
+                      verbose=args.verbose)
+print("Done.")
+
+utils.log_timestamp()
+print("Running evaluation...")
+model.test(test_sequence, verbose=True)
+print("Done.")
+utils.log_timestamp()
