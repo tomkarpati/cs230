01e66fb23a04c5cdf7b41c90b3286b101444f30e
Thomas Karpati <tkarpati@foodclaw.net>
2019-06-07 11:29:54 -0700

    Adding data analysis utils
    Adding support for getting data/filename/label information all together
    Adding utils for waveform and spectrogram visualization

--- data_processing/utils.py
+++ data_processing/utils.py
@@ -6,9 +6,23 @@ def log_timestamp():
   print("=== TIMESTAMP === :", datetime.datetime.now())
 
 
-def visualize_spectrogram(data_sequence, batch, index):
-  x,y = data_sequence.__getitem__(batch)
-  print("Current sample: ", ID2LABEL(np.argmax(y[index])))
-  plt.pcolormesh(x[index,:,:,0])
+def visualize_waveform(data):
+  plt.plot(data)
+  plt.xlabel('Time (samples)')
+  plt.ylabel('Amplitude')
+  plt.title('Audio Waveform')
+  plt.show()
+  
+
+def visualize_spectrogram(data, label):
+  print("Current sample: ", label)
+  fig, a = plt.subplots()
+  im = a.pcolormesh(data)
+  plt.xlabel('Time (window index)')
+  plt.ylabel('Frequency')
+
+  fig.colorbar(im, ax=a, orientation='vertical')
+  
+  plt.title('Audio Spectrogram')
   plt.show()
 
