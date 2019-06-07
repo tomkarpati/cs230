20550d25922fa34c5f728cbb729ce166d0a3e914
Thomas Karpati <tkarpati@foodclaw.net>
2019-06-07 11:30:37 -0700

    Adding interactive shell file

---
+++ interactive.py
@@ -0,0 +1,11 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from data_processing import generate_data_sets
+from data_processing import keras_data_generator
+from data_processing import utils
+
+from keras_models import keras_model
+
+exit()
